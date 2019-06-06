901c704518ddabc6c4b81a92d99aa2e39522f375
Antonio Blandin <antonio.blandin25@gmail.com>
2019-06-06 20:11:25 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,15 @@
 class User
   attr_accessor :first_name, :last_name
+  @@all=[]
+  
+  def initialize (first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+    @@all << self
+  end
+  
+  def self. all
+    @@all
+  end 
+  
 end
