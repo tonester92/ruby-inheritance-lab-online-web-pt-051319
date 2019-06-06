c1c095cb67eac07e791897d80cdc3a08adea52fe
Antonio Blandin <antonio.blandin25@gmail.com>
2019-06-06 20:21:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,12 @@
 class Student < User
   attr_accessor :knowledge 
   
+  def learn(string) 
+    knowledge << string
+    end
+  
   def initialize
     @knowledge = [] 
   end 
   
-  def learn(string) 
-    knowledge << string
-    end
 end
