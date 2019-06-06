e9d7574210265fd0c27e5f0267457856dc8ac639
Antonio Blandin <antonio.blandin25@gmail.com>
2019-06-06 20:22:13 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
-
+class Student < User
+  attr_accessor :knowledge 
+  
+  def learn(string) 
+    knowledge << string
+    end
+  
+  def initialize
+    @knowledge = [] 
+  end 
+  
 end
